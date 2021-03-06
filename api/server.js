2b3bb5835dd512c614cfaec81ffed354f1c81347
Pete VS <pete_vs88@me.com>
2021-03-06 08:19:40 -0500

    connect resource and tasks router

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const helmet = require('helmet')
 const projectsRouter = require('./project/router')
+const resourcesRouter = require('./resource/router')
+const tasksRouter = require('./task/router')
 
 const server = express()
 
@@ -14,5 +16,7 @@ server.get('/', ( req, res ) => {
     })
 })
 server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
 
 module.exports = server
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,15 @@
 // build your `/api/tasks` router here
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async ( req, res, next ) => {
+    try {
+        res.json({message: 'I am the tasks router'})
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
