ec8e90fcdc61a4efbaba5f5f819faad957e1162f
Pete VS <pete_vs88@me.com>
2021-03-06 07:57:10 -0500

    initial project table

--- data/migrations/20210306124856_projects.js
+++ data/migrations/20210306124856_projects.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex) {
+exports.up = async function(knex) {
   
+    await knex.schema.createTable("projects", (table) => {
+        table.increments("project_id")
+        table.text("project_name").notNull()
+        table.text("project_description")
+        table.integer("project_completed")
+    })
+
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("projects")
 };
